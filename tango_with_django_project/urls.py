25aad29684ba08654cbbdf035bd997b85f0b04b0
“Ethan <2794053b@student.gla.ac.uk>
2025-02-13 18:37:38 +0000

    Chapter 6

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 path('', views.index, name='index'),
 path('about/', views.about, name='about'),
+path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 path('rango/', include('rango.urls')),
 # The above maps any URLs starting with rango/ to be handled by rango.
 path('admin/', admin.site.urls),
