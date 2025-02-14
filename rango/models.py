17e6b14dcf1e81f188b4ca254d1bc1749bf7defd
“Ethan <2794053b@student.gla.ac.uk>
2025-02-14 03:01:22 +0000

    Chapter 7

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    nameMaxLength = 128
+    name = models.CharField(max_length=nameMaxLength, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -17,7 +18,8 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    titleMaxLength = 128
+    title = models.CharField(max_length=titleMaxLength)
     url = models.URLField()
     views = models.IntegerField(default=0)
     def __str__(self):
