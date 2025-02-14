f9d57feecc0876fe2797874422887927ecc95643
“Ethan <2794053b@student.gla.ac.uk>
2025-02-14 14:03:55 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+LOGIN_URL ='rango:login'
 
 
 # Quick-start development settings - unsuitable for production
@@ -103,6 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher'
+,
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+,
+)
 
 
 # Internationalization
